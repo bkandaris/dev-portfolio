d2369909730fa73ebf89de52a4bb7f57b4eacf22
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-10-14 20:45:50 -0400

    email form working, needs styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Contact from './components/Contact';
 
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,33 @@
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
+
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('gmail', 'template_h8l5byv', form.current, 'user_Zod23S6fYb9xLRNmu8Yz7')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+  };
+ 
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
+
+export default Contact;
