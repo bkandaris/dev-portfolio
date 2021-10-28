71a74f8b028d34d6addd7cf8e96df3e1e4f19adb
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-10-28 18:33:35 -0400

    about-me section pretty much finished and responsive

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,20 +5,19 @@ const AboutMe = () => {
     <div className='about-wrapper'>
       <div className='about-info-wrapper'>
         <div className='about-left'>
-          <h3 className='about-header'>About Me</h3>
+          <h3 className='about-header-left'>About Me</h3>
           <p className='about-text'>
             Hello! My name is Ben and I enjoy web development. Starting in 2018
-            I discovered the magic of turning code into web pages.
+            I discovered the magic of turning code into web applications.
           </p>
           <p className='about-text'>
             After self-teaching for a few months, I decided to join a web
-            development bootcamp to improve my skills. Now that I have finished
-            the bootcamp I look forward to starting a career in web development!
+            development and computer science bootcamp to improve my skills. Now that I have finished I look forward to starting a career in web development!
           </p>
         </div>
         <div className='about-right'>
-          <h3 className='about-header'>Skills</h3>
-          <ul>
+          <h3 className='about-header-right'>Skills</h3>
+          <ul className="skills">
             <li>HTML</li>
             <li>CSS</li>
             <li>SCSS</li>
