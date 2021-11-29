cf72b69432d43ed135fa8ded4545af7837b0bd47
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-11-29 12:02:40 -0500

    fixed some styling

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,9 +1,7 @@
 import React from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faGithub } from '@fortawesome/free-brands-svg-icons';
-{
-  /* <FontAwesomeIcon icon={faGithub} /> */
-}
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
 
 const ProjectCard = (props) => {
   return (
@@ -17,9 +15,16 @@ const ProjectCard = (props) => {
       <div className='project-desc-wrapper'>
         <h3>{props.projectTitle}</h3>
         <p>{props.projectDescription}</p>
-        {props.technologiesUsed.map((item, index) => {
-          return <p key={index}>{item}</p>;
-        })}
+        <h3>Technologies Used</h3>
+        <div className='tech-used'>
+          {props.technologiesUsed.map((item, index) => {
+            return <p key={index}>{item}</p>;
+          })}
+        </div>
+        <div className='icon-wrapper'>
+          <FontAwesomeIcon icon={faGithub} size='2x' />
+          <FontAwesomeIcon icon={faLink} size='2x' />
+        </div>
       </div>
     </div>
   );
--- src/components/listedprojects.js
+++ src/components/listedprojects.js
@@ -5,7 +5,8 @@ const projectsArray = [
       'https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80',
     projectImageAlt: 'dog',
     projectDescription:
-      'This is the first description of a project. I used these specific technologies to create this project',
+      'This is the first description of a project. I used these specific technologies to create this project I used these specific technologies to create this project d these specific technologies to create this project',
+    hostedAt: 'https://unsplash.com/s/photos/dogs',
     hostedAt: 'https://unsplash.com/s/photos/dogs',
     githubLink: 'https://github.com/',
     technologiesUsed: ['HTML', 'CSS', 'React', 'Node.js', 'Express'],
@@ -16,7 +17,8 @@ const projectsArray = [
       'https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80',
     projectImageAlt: 'dog',
     projectDescription:
-      'This is the first description of a project. I used these specific technologies to create this project',
+      'This is the first description of a project. I used these specific technologies to create this project I used these specific technologies to create this project',
+    hostedAt: 'https://unsplash.com/s/photos/dogs',
     hostedAt: 'https://unsplash.com/s/photos/dogs',
     githubLink: 'https://github.com/',
     technologiesUsed: ['HTML', 'CSS', 'React', 'Node.js', 'Express'],
@@ -27,7 +29,7 @@ const projectsArray = [
       'https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80',
     projectImageAlt: 'dog',
     projectDescription:
-      'This is the first description of a project. I used these specific technologies to create this project',
+      'This is the first description of a project. I used these specific technologies to create this project. This is the first description of a project. I used these specific technologies to create this project',
     hostedAt: 'https://unsplash.com/s/photos/dogs',
     githubLink: 'https://github.com/',
     technologiesUsed: ['HTML', 'CSS', 'React', 'Node.js', 'Express'],
