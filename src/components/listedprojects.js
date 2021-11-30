1f34d0b828524d5512a674e91906dd4e7609c702
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-11-29 21:01:19 -0500

    updated projects

--- src/components/listedprojects.js
+++ src/components/listedprojects.js
@@ -2,7 +2,7 @@ const projectsArray = [
   {
     projectTitle: 'JavaScript Quiz Application',
     projectImage: 'https://i.gyazo.com/702bf26869ed1b8cd8e3c98eea09b302.png',
-    projectImageAlt: 'quiz app picture',
+    projectImageAlt: 'quiz app',
     projectDescription:
       'A full stack quiz application that the admin can handle CRUD operations for all of the questions. The user can learn more about JavaScript through a fun quiz game that tracks their score.',
     hostedAt: 'https://javascriptquizapplication.netlify.app/',
@@ -10,27 +10,24 @@ const projectsArray = [
     technologiesUsed: ['React', 'Redux', 'CSS', 'Node.js', 'Express'],
   },
   {
-    projectTitle: 'First Project',
-    projectImage:
-      'https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80',
-    projectImageAlt: 'dog',
+    projectTitle: 'Stock Market Tracker',
+    projectImage: 'https://i.gyazo.com/ae1c8a8f918d0c194adeffcbb61ec771.png',
+    projectImageAlt: 'stock app',
     projectDescription:
-      'This is the first description of a project. I used these specific technologies to create this project I used these specific technologies to create this project',
-    hostedAt: 'https://unsplash.com/s/photos/dogs',
-    hostedAt: 'https://unsplash.com/s/photos/dogs',
-    githubLink: 'https://github.com/',
-    technologiesUsed: ['HTML', 'CSS', 'React', 'Node.js', 'Express'],
+      'An application that uses a third party API to pull in stock data. The user can type in a stock ticker and have the price history displayed on chart. ',
+    hostedAt: 'https://stocktrackerapplication.netlify.app/',
+    githubLink: 'https://github.com/bkandaris/stock-app',
+    technologiesUsed: ['React', 'CSS', 'Axios'],
   },
   {
-    projectTitle: 'First Project',
-    projectImage:
-      'https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80',
-    projectImageAlt: 'dog',
+    projectTitle: "Drink Ingredient's application",
+    projectImage: 'https://i.gyazo.com/7d6ef98c005d0df7aa95333339201214.png',
+    projectImageAlt: 'bar',
     projectDescription:
-      'This is the first description of a project. I used these specific technologies to create this project. This is the first description of a project. I used these specific technologies to create this project',
-    hostedAt: 'https://unsplash.com/s/photos/dogs',
-    githubLink: 'https://github.com/',
-    technologiesUsed: ['HTML', 'CSS', 'React', 'Node.js', 'Express'],
+      'A fun project using a third party API where users can look through a list of drinks to try or search for one.',
+    hostedAt: 'https://drinkingredients.netlify.app',
+    githubLink: 'https://github.com/bkandaris/bartender-app',
+    technologiesUsed: ['React', 'CSS', 'Context API'],
   },
 ];
 
